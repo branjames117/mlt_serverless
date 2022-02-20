31f085ab07d524d0f7646c73ee45fbc883766d4b
Brandon Hoskins <branjames117@gmail.com>
2022-02-20 13:16:52 -0600

    added service worker for caching and manifest.json

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const APP_PREFIX = 'MLTArts-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  '/',
+  './index.html',
+  './about-us.html',
+  './business-membership.html',
+  './couple-membership.html',
+  './family-membership.html',
+  './history.html',
+  './individual-membership.html',
+  './membership.html',
+  './oops.html',
+  './thank-you.html',
+  './ticket-policy.html',
+  './public/css/style.css',
+  './public/js/form_script.js',
+  './public/js/history_script.js',
+  './public/js/index_script.js',
+  './public/js/script.js',
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      return request || fetch(e.request);
+    })
+  );
+});
